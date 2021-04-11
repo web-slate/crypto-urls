59dd729e518859fefb6a669363a7c1ddf1759362
Venkat.R <ramsunvtech@gmail.com>
2021-04-11 16:37:10 +0800

    tech(base): Add url-parse module

--- src/encrypt.js
+++ src/encrypt.js
@@ -1,5 +1,12 @@
-const encrypt = (uri) => {
+const Url = require('url-parse');
+
+const encrypt = (givenUrl, {
+  pattern
+}) => {
   // mocked for initial test.
+  const parsedURL = Url(givenUrl)
+  const { pathname } = parsedURL
+  console.log('parsedURL: ', parsedURL)
   return 'https://testapp.com/modules/provider/WTq8zYcZfaWVvMncigHqwQ=='
 };
 
