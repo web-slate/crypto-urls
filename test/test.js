e1b24e2cbe37745222c81f7097ea8b529de26755
Shankar <shangan.23@gmail.com>
2021-04-13 23:08:42 +0530

    test(encrypt-decrypt): add testcase for encrypt and decrypt

---
+++ test/test.js
@@ -0,0 +1,61 @@
+const { encrypt, decrypt } = require('../src')
+
+describe('Success case: Crypto Url Encrypt and Decrypt with sequential pattern', () => {
+  let actualUrl =
+    'https://testapp.com/modules/provider/7d8bb2ff-04d8-4c3c-96c4-faafb6321432/view/tasks'
+  let patternUrl = 'https://testapp.com/modules/provider/:id/:action/tasks'
+  var encryptedUrl
+
+  test('encrypt', () => {
+    encryptedUrl = encrypt(actualUrl, {
+      pattern: patternUrl,
+    })
+  })
+
+  test('decrypt', () => {
+    expect(
+      decrypt(encryptedUrl, {
+        pattern: patternUrl,
+      })
+    ).toEqual(actualUrl)
+  })
+})
+
+describe('Success case: Crypto Url Encrypt and Decrypt with mixed/non-sequential pattern', () => {
+  let actualUrl =
+    'https://testapp.com/modules/provider/7d8bb2ff-04d8-4c3c-96c4-faafb6321432/tasks/view'
+  let patternUrl = 'https://testapp.com/modules/provider/:id/tasks/:action'
+  var encryptedUrl
+
+  test('encrypt', () => {
+    encryptedUrl = encrypt(actualUrl, {
+      pattern: patternUrl,
+    })
+  })
+
+  test('decrypt', () => {
+    expect(
+      decrypt(encryptedUrl, {
+        pattern: patternUrl,
+      })
+    ).toEqual(actualUrl)
+  })
+})
+
+describe('Failure case: Crypto Url Encrypt and Decrypt without pattern option', () => {
+  test('encrypt', () => {
+    expect(() =>
+      encrypt(
+        'https://testapp.com/modules/provider/7d8bb2ff-04d8-4c3c-96c4-faafb6321432/tasks/view'
+      )
+    ).toThrow('pattern is missing')
+  })
+
+  test('decrypt', () => {
+    expect(() =>
+      decrypt(
+        'https://testapp.com/modules/provider/7d8bb2ff-04d8-4c3c-96c4-faafb6321432/tasks/view'
+      )
+    ).toThrow('pattern is missing')
+  })
+})
