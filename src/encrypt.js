ad0541c014705e4834a88d8a58221b8523cd23c0
Shankar <shangan.23@gmail.com>
2021-04-13 19:38:29 +0530

    tech(encrypt): review comments incorporated

--- src/encrypt.js
+++ src/encrypt.js
@@ -12,19 +12,19 @@ const encrypt = (givenUrl, options = {} ) => {
   const parsedPatternURL = Url(pattern);
   const parsedGivenURL = Url(givenUrl);
 
-  const patternPathnameArray = parsedPatternURL.pathname.split("/");
-  const givenURLPathnameArray = parsedGivenURL.pathname.split("/");
+  const patternPathChunks = parsedPatternURL.pathname.split("/");
+  const givenPathChunks = parsedGivenURL.pathname.split("/");
 
-  const filteredPatterns = patternPathnameArray.filter((param)=> param.indexOf(':') === 0);
+  const filteredPatterns = patternPathChunks.filter((param)=> param.indexOf(':') === 0);
 
   filteredPatterns.map((p) => { 
-    let encryptedString = CryptoJS.AES.encrypt(givenURLPathnameArray[patternPathnameArray.indexOf(p)],secret).toString();
+    let encryptedString = CryptoJS.AES.encrypt(givenPathChunks[patternPathChunks.indexOf(p)],secret).toString();
     //to escape `/` (forward slash) from encryption
     let escapedEncryptedString = encryptedString.replace(/\+/g,'p1L2u3S').replace(/\//g,'s1L2a3S4h').replace(/=/g,'e1Q2u3A4l');
-    givenURLPathnameArray[patternPathnameArray.indexOf(p)] = escapedEncryptedString
+    givenPathChunks[patternPathChunks.indexOf(p)] = escapedEncryptedString
   })
 
-  return `${parsedGivenURL.origin}${givenURLPathnameArray.join('/')}`
+  return `${parsedGivenURL.origin}${givenPathChunks.join('/')}`
 };
 
 export { encrypt };
