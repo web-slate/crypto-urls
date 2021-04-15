e909325d10a7a68be733c5a3d7605f41c25bb158
Shankar <shangan.23@gmail.com>
2021-04-15 15:45:50 +0530

    tech(decrypt): enhance path with decrypt method

--- test/url.test.js
+++ test/url.test.js
@@ -14,7 +14,7 @@ describe('Success case: Crypto Url Encrypt and Decrypt with sequential pattern',
 
   test('decrypt', () => {
     expect(
-      decrypt(encryptedUrl, {
+      decrypt.url(encryptedUrl, {
         pattern: patternUrl,
       })
     ).toEqual(actualUrl)
@@ -35,7 +35,7 @@ describe('Success case: Crypto Url Encrypt and Decrypt with mixed/non-sequential
 
   test('decrypt', () => {
     expect(
-      decrypt(encryptedUrl, {
+      decrypt.url(encryptedUrl, {
         pattern: patternUrl,
       })
     ).toEqual(actualUrl)
@@ -53,7 +53,7 @@ describe('Failure case: Crypto Url Encrypt and Decrypt without pattern option', 
 
   test('decrypt', () => {
     expect(() =>
-      decrypt(
+      decrypt.url(
         'https://testapp.com/modules/provider/7d8bb2ff-04d8-4c3c-96c4-faafb6321432/tasks/view'
       )
     ).toThrow('pattern is missing')
