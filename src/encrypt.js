11335d9c70093246dd1a828f3bf2d790cf8e74ef
Shankar <shangan.23@gmail.com>
2021-04-19 17:49:55 +0530

    fix(token): unkown token fix

--- src/encrypt.js
+++ src/encrypt.js
@@ -1,12 +1,44 @@
 const Url = require('url-parse')
 const CryptoJS = require('crypto-js')
-const {
-  isValidUrl,
-  isValidPatternUrl,
-  isValidPatterObject,
-  isValidPattern,
-  isValidPath,
-} = require('../util')
+
+const isValidUrl = (url) => {
+  var res = url.match(
+    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+  )
+  return res !== null
+}
+
+const isValidPatternUrl = (url) => {
+  if (isValidUrl(url)) {
+    const parsedURL = Url(url)
+    const urlChunks = parsedURL.pathname.split('/')
+    const filteredPatterns = filterPatterns(urlChunks)
+    return filteredPatterns.length > 0
+  }
+  return false
+}
+
+const filterPatterns = (chunks) => {
+  return chunks.filter((param) => param.indexOf(':') === 0)
+}
+
+const isValidPatterObject = (obj) => {
+  let patternArray = []
+  const objKeys = Object.keys(obj)
+  objKeys.filter((i) => {
+    if (i.startsWith(':')) patternArray.push(i)
+  })
+  return patternArray.length === objKeys.length
+}
+
+const isValidPath = (path) => {
+  return path.startsWith('/')
+}
+
+const isValidPattern = (chunks) => {
+  let filteredPatterns = filterPatterns(chunks)
+  return filteredPatterns.length > 0
+}
 
 const encrypt = (function () {
   const secret = 'Secret Passphrase'
